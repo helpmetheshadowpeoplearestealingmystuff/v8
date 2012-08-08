helpmetheshadowpeoplearestealingmystuff/v8

commit 6987e86f3c10901c34354266c654dc108c0fed0c

Add heap space sizes and external memory counter to V8 statistics extension.
R=yangguo@chromium.org

Review URL: https://chromiumcodereview.appspot.com/10823202

git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@12272 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

src/extensions/statistics-extension.cc
@@ -41,13 +41,19 @@ v8::Handle<v8::FunctionTemplate> StatisticsExtension::GetNativeFunction(
 }
 
 
-static void AddProperty(v8::Local<v8::Object> object,
-                        StatsCounter* counter,
-                        const char* name) {
+static void AddCounter(v8::Local<v8::Object> object,
+                       StatsCounter* counter,
+                       const char* name) {
   if (counter->Enabled()) {
     object->Set(v8::String::New(name),
                 v8::Number::New(*counter->GetInternalPointer()));
   }
+}
+
+static void AddNumber(v8::Local<v8::Object> object,
+                      double value,
+                      const char* name) {
+  object->Set(v8::String::New(name), v8::Number::New(value));
 }
 
 
@@ -65,35 +71,75 @@ v8::Handle<v8::Value> StatisticsExtension::GetCounters(
   Counters* counters = isolate->counters();
   v8::Local<v8::Object> result = v8::Object::New();
 
-#define ADD_COUNTER(name, caption)                                             \
-  AddProperty(result, counters->name(), #name);
+#define ADD_COUNTER(name, caption)                                            \
+  AddCounter(result, counters->name(), #name);
 
   STATS_COUNTER_LIST_1(ADD_COUNTER)
   STATS_COUNTER_LIST_2(ADD_COUNTER)
 #undef ADD_COUNTER
-#define ADD_COUNTER(name)                                                      \
-  AddProperty(result, counters->count_of_##name(), "count_of_" #name);         \
-  AddProperty(result, counters->size_of_##name(),  "size_of_" #name);
+#define ADD_COUNTER(name)                                                     \
+  AddCounter(result, counters->count_of_##name(), "count_of_" #name);         \
+  AddCounter(result, counters->size_of_##name(),  "size_of_" #name);
 
   INSTANCE_TYPE_LIST(ADD_COUNTER)
 #undef ADD_COUNTER
-#define ADD_COUNTER(name)                                                      \
-  AddProperty(result, counters->count_of_CODE_TYPE_##name(),                   \
-              "count_of_CODE_TYPE_" #name);                                    \
-  AddProperty(result, counters->size_of_CODE_TYPE_##name(),                    \
-              "size_of_CODE_TYPE_" #name);
+#define ADD_COUNTER(name)                                                     \
+  AddCounter(result, counters->count_of_CODE_TYPE_##name(),                   \
+             "count_of_CODE_TYPE_" #name);                                    \
+  AddCounter(result, counters->size_of_CODE_TYPE_##name(),                    \
+             "size_of_CODE_TYPE_" #name);
 
   CODE_KIND_LIST(ADD_COUNTER)
 #undef ADD_COUNTER
-#define ADD_COUNTER(name)                                                      \
-  AddProperty(result, counters->count_of_FIXED_ARRAY_##name(),                 \
-              "count_of_FIXED_ARRAY_" #name);                                  \
-  AddProperty(result, counters->size_of_FIXED_ARRAY_##name(),                  \
-              "size_of_FIXED_ARRAY_" #name);
+#define ADD_COUNTER(name)                                                     \
+  AddCounter(result, counters->count_of_FIXED_ARRAY_##name(),                 \
+             "count_of_FIXED_ARRAY_" #name);                                  \
+  AddCounter(result, counters->size_of_FIXED_ARRAY_##name(),                  \
+             "size_of_FIXED_ARRAY_" #name);
 
   FIXED_ARRAY_SUB_INSTANCE_TYPE_LIST(ADD_COUNTER)
 #undef ADD_COUNTER
 
+  AddNumber(result, isolate->memory_allocator()->Size(),
+            "total_committed_bytes");
+  AddNumber(result, heap->new_space()->Size(),
+            "new_space_live_bytes");
+  AddNumber(result, heap->new_space()->Available(),
+            "new_space_available_bytes");
+  AddNumber(result, heap->new_space()->CommittedMemory(),
+            "new_space_commited_bytes");
+  AddNumber(result, heap->old_pointer_space()->Size(),
+            "old_pointer_space_live_bytes");
+  AddNumber(result, heap->old_pointer_space()->Available(),
+            "old_pointer_space_available_bytes");
+  AddNumber(result, heap->old_pointer_space()->CommittedMemory(),
+            "old_pointer_space_commited_bytes");
+  AddNumber(result, heap->old_data_space()->Size(),
+            "old_data_space_live_bytes");
+  AddNumber(result, heap->old_data_space()->Available(),
+            "old_data_space_available_bytes");
+  AddNumber(result, heap->old_data_space()->CommittedMemory(),
+            "old_data_space_commited_bytes");
+  AddNumber(result, heap->code_space()->Size(),
+            "code_space_live_bytes");
+  AddNumber(result, heap->code_space()->Available(),
+            "code_space_available_bytes");
+  AddNumber(result, heap->code_space()->CommittedMemory(),
+            "code_space_commited_bytes");
+  AddNumber(result, heap->cell_space()->Size(),
+            "cell_space_live_bytes");
+  AddNumber(result, heap->cell_space()->Available(),
+            "cell_space_available_bytes");
+  AddNumber(result, heap->cell_space()->CommittedMemory(),
+            "cell_space_commited_bytes");
+  AddNumber(result, heap->lo_space()->Size(),
+            "lo_space_live_bytes");
+  AddNumber(result, heap->lo_space()->Available(),
+            "lo_space_available_bytes");
+  AddNumber(result, heap->lo_space()->CommittedMemory(),
+            "lo_space_commited_bytes");
+  AddNumber(result, heap->amount_of_external_allocated_memory(),
+            "amount_of_external_allocated_memory");
   return result;
 }
 

src/heap.h
@@ -1597,6 +1597,10 @@ class Heap {
     global_ic_age_ = (global_ic_age_ + 1) & SharedFunctionInfo::ICAgeBits::kMax;
   }
 
+  intptr_t amount_of_external_allocated_memory() {
+    return amount_of_external_allocated_memory_;
+  }
+
   // ObjectStats are kept in two arrays, counts and sizes. Related stats are
   // stored in a contiguous linear buffer. Stats groups are stored one after
   // another.

commit 5b7d382741470d5a4f5e8e78cd71e1d7ab039423

Fix Win64 compile error caused by r12272.
R=mstarzinger@chromium.org

Review URL: https://chromiumcodereview.appspot.com/10830213

git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@12276 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

src/extensions/statistics-extension.cc
@@ -51,9 +51,10 @@ static void AddCounter(v8::Local<v8::Object> object,
 }
 
 static void AddNumber(v8::Local<v8::Object> object,
-                      double value,
+                      intptr_t value,
                       const char* name) {
-  object->Set(v8::String::New(name), v8::Number::New(value));
+  object->Set(v8::String::New(name),
+              v8::Number::New(static_cast<double>(value)));
 }
 
 